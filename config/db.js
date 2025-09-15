cf95bb0473767eab5c2fe0fc288039235ceff1d4
Sreexcerpt <sree.excerpt@gmail.com>
2025-09-15 14:42:04 +0530

    SDfds

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(`mongodb+srv://excerpterp:excerpterp2025@cluster0.anwegvj.mongodb.net/ERP`);
+    const conn = await mongoose.connect(`mongodb+srv://excerpterp:excerpterp2025@cluster0.anwegvj.mongodb.net/demoerp`);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
